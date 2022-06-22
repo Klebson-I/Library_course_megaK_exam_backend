a86daa7d8d1c955f8ef86eae561fb687a466ad15
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-06-22 12:10:55 +0200

    creates foo to generate login and password // comment for last commit

--- routes/TokenRouter.ts
+++ routes/TokenRouter.ts
@@ -4,6 +4,7 @@ import {createAdminToken} from "../utils/usersActions";
 export const TokenRouter = Router();
 
 TokenRouter
+    //
     .get('/', async (req: Request, res: Response) => {
         const tokenObject = createAdminToken();
         res.json(tokenObject);
