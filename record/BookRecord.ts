62b976495656f0122dcbc7ac81cf5f846232bdfb
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-06-22 17:00:17 +0200

    create static functions for BookRecord

--- record/BookRecord.ts
+++ record/BookRecord.ts
@@ -1,6 +1,9 @@
 import {BookEntity, BookGenre} from "../utils/types";
 import {v4 as uuid} from 'uuid';
 import {pool} from "../utils/db";
+import {FieldPacket} from "mysql2";
+
+type BookResults = [BookEntity[], FieldPacket[]];
 
 export class BookRecord implements BookEntity {
     id?: string;
@@ -50,6 +53,37 @@ export class BookRecord implements BookEntity {
             await pool.execute("DELETE FROM `books` WHERE `id`=:id", {
                 id,
             })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    static async getOne(id: string): Promise<BookRecord | null> {
+        try {
+            const [results] = await pool.execute("SELECT * FROM `books` where `id` = :id", {
+                id
+            }) as BookResults;
+            return results.length !== 0 ? new BookRecord(results[0]) : null;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    static async getOneByTitle(title: string): Promise<BookRecord[] | null> {
+        const str = "'" + title + "%'";
+        try {
+            const [results] = await pool.execute("SELECT * FROM `books` where `title` LIKE" + str) as BookResults;
+
+            return results.length !== 0 ? results.map(book => new BookRecord(book)) : null;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    static async getAll(): Promise<BookRecord[] | null> {
+        try {
+            const [results] = await pool.execute("SELECT * FROM `books`") as BookResults;
+            return results.length !== 0 ? results.map(book => new BookRecord(book)) : null;
         } catch (e) {
             console.log(e);
         }
