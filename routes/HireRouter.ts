e3e24c6b2d45e9b7cb84f1bb888857acf063c4c9
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-06-24 11:45:54 +0200

    add get and delete method handle to Hire router

--- routes/HireRouter.ts
+++ routes/HireRouter.ts
@@ -1,5 +1,5 @@
 import {Request, Response, Router} from 'express';
-import {evaluateDate} from "../utils/utils";
+import {changeBookAmount, evaluateDate} from "../utils/utils";
 import {HireRecord} from "../record/HireRecord";
 
 export const HireRouter = Router();
@@ -22,8 +22,38 @@ HireRouter
             })
 
             const hire_id = await hire.insert();
-            res.status(200).json(hire_id);
+
+            if (hire_id) {
+                await changeBookAmount(book_id);
+                res.status(200).json(hire_id);
+            } else {
+                res.status(400).json(null);
+            }
+
         } catch (e) {
             console.log(e);
+        }
+    })
+    .get('/', async (req: Request, res: Response) => {
+        const hires = await HireRecord.getAll();
+        res.json(hires);
+    })
+    .get('/:id', async (req: Request, res: Response) => {
+        const hires = await HireRecord.getAll();
+        const userHires = hires.filter(hire => hire.user_id === req.params.id);
+        res.json(userHires);
+    })
+    .get('/check/:user_id/:book_id', async (req: Request, res: Response) => {
+        const haveBook = await HireRecord.checkIfUserHaveBook(req.params.user_id, req.params.book_id);
+        res.json(haveBook);
+    })
+    .delete('/:id', async (req: Request, res: Response) => {
+        const hire = await HireRecord.getOne(req.params.id);
+        if (hire) {
+            //@TODO add book amount +1 when someone gives it back
+            await hire.delete();
+            res.json(true);
+        } else {
+            res.json(false);
         }
     })
